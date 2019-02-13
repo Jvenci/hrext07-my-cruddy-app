b72dc921b563470f551957296e3f8835f6df0c84
Julian V <jvenci@comcast.net>
2019-02-13 00:13:39 -0800

    refactor of base code

--- app/app.js
+++ app/app.js
@@ -7,86 +7,73 @@ interact with localstorage
 
 $(document).ready(function(){
   // this is where we jquery
-  //var nameData = 'ourKey'; // going to need to make this dynamic?
+  //var firstName = 'ourKey'; // going to need to make this dynamic?
   
 
-  var contactConstructor = function(name, phone, address, email) {
+  var contactConstructor = function(fN, lN,  phone, address, email) {
     var person = { 
-      name: name,
+      firstName: fN,
+      lastName: lN,
       phone: phone,
       address: address,
       email: email
     }
-    return JSON.stringify(person);
+    return person;
   };
 
-  $('.btn-add').on('click', function(e){
-    console.log(e);
-    var nameData = $('.input-name').val();
+  //add entry to db and address table
+  $('.btn-add').on('click', function(){
+       
+    var firstName = $('.input-firstName').val();
+    var lastName = $('.input-lastName').val();
+    var lastName = $('.input-lastName').val();
     var phoneData = $('.input-phone').val();
     var addressData = $('.input-address').val();
     var emailData = $('.input-email').val();
-    // write to db
-    localStorage.setItem(nameData, contactConstructor(nameData, phoneData, addressData, emailData));
-    // read from db
-    //var displayText = nameData + ' | ' + localStorage.getItem(nameData);
-    // this only displays the last one? might want to switch to html
-    // and append a div
-    // <div class="display-data-item" data-keyValue="nameData">phoneData</div>
-    // if you use backticks ` you can use ${templateLiterals}
-    // TODO make this vars make sense across the app
-    $('.container-data').append('<div class="display-data-item" data-keyValue="'+ nameData +'">'+nameData+ ' ' +phoneData+ ' ' +addressData+ ' ' +emailData+'</div>');
-    $('.input-name').val('');
-    $('.input-phone').val('');
-    $('.input-address').val('');
-    $('.input-email').val('');
+
+    var personObj = contactConstructor(firstName, lastName, phoneData, addressData, emailData)
+    
+    localStorage.setItem(localStorage.length + 1,JSON.stringify(personObj));
+    
+    $('.container-form').children('input').val('')
   });
 
 
   // update db
   $('.btn-update').on('click', function() {
-    var nameData = $('.input-name').val();
+    var firstName = $('.input-firstName').val();
+    var lastName = $('.input-lastName').val();
     var phoneData = $('.input-phone').val();
     var addressData = $('.input-address').val();
     var emailData = $('.input-email').val();
 
-    var inputArr = [nameData, phoneData, addressData, emailData];
-    var personInfo = JSON.parse(localStorage.getItem(nameData))
-    var properties = Object.keys(personInfo)
+    var inputArr = [firstName, phoneData, addressData, emailData];
+    var personInfo = JSON.parse(ID)
+    var modifiableFields = Object.keys(personInfo)
 
     for (var i = 0; i < inputArr.length; i++) {
       if (inputArr[i] !== '') {
-        personInfo[properties[i]] = inputArr[i];
+        personInfo[modifiableFields[i]] = inputArr[i];
         
       }
     }
 
-    localStorage.setItem(nameData, JSON.stringify(personInfo))
-    $('.input-name').val('');
-    $('.input-phone').val('');
-    $('.input-address').val('');
-    $('.input-email').val('');
+    localStorage.setItem(ID, JSON.stringify(personInfo))
+   $('.container-form').children('input').val('')
   });
 
   // delete item
   $('.btn-delete').on('click', function(){
-    var nameData = $('.input-name').val();
+    var firstName = $('.input-firstName').val();
+    var lastName = $('.input-lastName').val();
     var phoneData = $('.input-phone').val();
     var addressData = $('.input-address').val();
     var emailData = $('.input-email').val();
     // console.log(e.currentTarget.dataset.keyvalue);
-     //var nameData = $('.input-name').val();
-    localStorage.removeItem(nameData);
+     //var firstName = $('.input-firstName').val();
+    localStorage.removeItem(ID);
     //$('.container-data').text('');
-    $('.input-name').val('');
-    $('.input-phone').val('');
-    $('.input-address').val('');
-    $('.input-email').val('');
+    $('.container-form').children('input').val('')
   });
-  // delete all?
-  // $('.btn-clear').click(function(){
-  //   localStorage.clear();
-  //   $('.container-data').text('');
-  // });
 
 });
